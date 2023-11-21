4f9caa2eb584538f97eff25025cd13f871f7fb1c
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-21 12:30:32 -0500

    create methods from comtroller

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -4,32 +4,45 @@ namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-        //
+        $products =  Product::all();
+        return $products;
     }
 
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+        $product->description = $request->description;
+        $product->description = $request->price;
+        $product->description = $request->stock;
+        $product->save();
     }
 
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+        return $product;
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::findOrFail($request->id);
+        $product->description = $request->description;
+        $product->description = $request->price;
+        $product->description = $request->stock;
+        $product->save();
+        return $product;
     }
 
     public function destroy($id)
     {
-        //
+        $roduct = Product::destroy($id);
+        return $product;
     }
 }
