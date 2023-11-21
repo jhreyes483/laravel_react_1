af559ab4d0f4b00f71cdb2b9f02520b8d3c38212
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-21 12:23:09 -0500

    Crear y ejecutar migraciones php artisan migrate

---
+++ app/Http/Controllers/Product/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
--- database/migrations/2023_11_21_170340_create_products_table.php
+++ database/migrations/2023_11_21_170340_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->double('price',8,2);
+            $table->integer('stock');
             $table->timestamps();
         });
     }
