d98035dc2dc3c7f31d028f910b074c3547b8bddb
Javier-Reyes-pet <javier.reyes@laika.pet>
2023-11-21 14:19:02 -0500

    create and execute showProdut

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// importar nuestos componentes
+import ShowProducts from './components/ShowProducts'; 
+import CreateProduct from './components/CreateProduct'; 
+import EditProduct from './components/EditProduct'; 
+
 
 function App() {
   return (
     <div className="App">
       <button className='btn btn-primary'>Crear</button>
-     
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <ShowProducts/> }/>
+          <Route path='/create'  element={ <CreateProduct/> } />
+          <Route/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- reactfront/src/components/ShowProducts.js
+++ reactfront/src/components/ShowProducts.js
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import React, { useEffect, useState} from "react";
-import axios from "axios";
-
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 import {Link} from 'react-router-dom'
 
 
 const endpoint = "http://localhost:8000/api"
+
 const ShowProducts = () => {
     const [products, setProducts] = useState([]) // inicializa la varible de estado 
     useEffect (()=>{
@@ -13,22 +12,22 @@ const ShowProducts = () => {
     }, [])
 
 
-    const getAllProducts = async () =>{
-        const response = await axios.get('${endpoint}/products')
-        setProducts(response)
-    }
-    const deleteProducts = async (id) =>{
-        await axios.delete('${endpoint}/product/${id}')
+    const getAllProducts = async () => {
+        const response = await axios.get(`${endpoint}/products`)
+        setProducts(response.data)
+        //console.log(response.data)
+      }
+    const deleteProduct = async (id) => {
+        await axios.delete(`${endpoint}/product/${id}`)
         getAllProducts()
     }
-
     return (
         <div>
             <div className="d-grid grap-2"> 
                 <Link to="/create" className="btn btn-success btn-lg mt-2 mb-2 text-white" >Crear</Link>
             </div>
 
-            <table className="table table-striped"></table>
+            <table className="table table-striped">
             <thead className="bg-primary text-white">
                 <tr>
                     <th> Descripcion</th>
@@ -38,18 +37,20 @@ const ShowProducts = () => {
                 </tr>
             </thead>
             <tbody>
-                { products.map( product ) => (
-                <tr key={product.id}>
-                    <td>{product.description}</td>
-                    <td>{product.price}</td>
-                    <td>{product.stock}</td>
-                    <td>
-                        <Link to={'/edit/${product.id}'} className="btn btn-warning">Editar</Link>
-                        <button onClick={ () => deleteProducts(product.id)} className="btn btn-danger">Delete</button>
-                    </td>
-                </tr>
+            { products.map( (product) => (
+                    <tr key={product.id}>
+                        <td> {product.description} </td>    
+                        <td> {product.price} </td>    
+                        <td> {product.stock} </td>    
+                        <td>
+                            <Link to={`/edit/${product.id}`} className='btn btn-warning'>Edit</Link>
+                            <button onClick={ ()=>deleteProduct(product.id) } className='btn btn-danger'>Delete</button>
+                        </td>
+
+                    </tr>
                 )) }
             </tbody>
+            </table>
 
         </div>
     )
